09310c3d4424e90a9c3ef604ee0fc332974a2a78
Aqsa <aqsa65020@gmail.com>
2017-12-09 12:24:58 +0500

    comit by aqsa

--- app/src/main/java/octapult/com/sazmaan/GraphActivity.java
+++ app/src/main/java/octapult/com/sazmaan/GraphActivity.java
@@ -3,11 +3,16 @@ package octapult.com.sazmaan;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class GraphActivity extends AppCompatActivity {
+import com.jjoe64.graphview.GraphView;
 
+public class GraphActivity extends AppCompatActivity {
+    GraphView graph ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
+
+        graph = (GraphView) findViewById(R.id.graph);
+
     }
 }
